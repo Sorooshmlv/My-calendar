9b130563581bc7df1dafc3662db2079a3a85c49b
SorooshMLV <SorooshMolavi@gmail.com>
2023-12-04 00:58:45 +0330

    style changes and deleting irrelevant options

--- app/Calendarone/page.tsx
+++ app/Calendarone/page.tsx
@@ -2,7 +2,7 @@
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin, {
-  Draggable,
+  // Draggable,
   DropArg,
 } from "@fullcalendar/interaction";
 import timeGridPlugin from "@fullcalendar/timegrid";
@@ -30,21 +30,6 @@ export default function Calendar1() {
     id: 0,
   });
 
-  useEffect(() => {
-    let draggableEl = document.getElementById("draggable-el");
-    if (draggableEl) {
-      new Draggable(draggableEl, {
-        itemSelector: ".fc-event",
-        eventData: function (eventEl) {
-          let title = eventEl.getAttribute("title");
-          let id = eventEl.getAttribute("data");
-          let start = eventEl.getAttribute("start");
-          return { title, id, start };
-        },
-      });
-    }
-  }, []);
-
   function handleDateClick(arg: { date: Date; allDay: boolean }) {
     setNewEvent({
       ...newEvent,
@@ -53,17 +38,6 @@ export default function Calendar1() {
       id: new Date().getTime(),
     });
     setShowModal(true);
-  }
-
-  function addEvent(data: DropArg) {
-    const event = {
-      ...newEvent,
-      start: data.date.toISOString(),
-      title: data.draggedEl.innerText,
-      allDay: data.allDay,
-      id: new Date().getTime(),
-    };
-    setAllEvents([...allEvents, event]);
   }
 
   function handleDeleteModal(data: { event: { id: string } }) {
@@ -115,27 +89,23 @@ export default function Calendar1() {
       <nav className="flex justify-between mb-12 border-b border-violet-100 p-4">
         <h1 className="font-bold text-2xl text-gray-700">Calendar</h1>
       </nav>
-      <div className=" min-h-screen p-12">
-        <div className="grid grid-cols-10">
-          <div className="col-span-8">
-            <FullCalendar
-              plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
-              headerToolbar={{
-                left: "prev,next today",
-                center: "title",
-                right: "dayGridMonth,timeGridWeek,timeGridDay",
-              }}
-              events={allEvents as EventSourceInput}
-              nowIndicator={true}
-              editable={true}
-              droppable={true}
-              selectable={true}
-              selectMirror={true}
-              dateClick={handleDateClick}
-              drop={(data) => addEvent(data)}
-              eventClick={(data) => handleDeleteModal(data)}
-            />
-          </div>
+      <div className=" min-h-screen p-20">
+        <div className="col-span-10">
+          <FullCalendar
+            plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
+            headerToolbar={{
+              left: "prev,next today",
+              center: "title",
+              right: "dayGridMonth,timeGridWeek,timeGridDay",
+            }}
+            events={allEvents as EventSourceInput}
+            nowIndicator={true}
+            droppable={true}
+            selectable={true}
+            selectMirror={true}
+            dateClick={handleDateClick}
+            eventClick={(data) => handleDeleteModal(data)}
+          />
         </div>
 
         <Transition.Root show={showDeleteModal} as={Fragment}>
@@ -197,7 +167,10 @@ export default function Calendar1() {
                         </div>
                       </div>
                     </div>
-                    <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                    <div
+                      className="bg-gray-50 px-4 py-3 sm:flex 
+                    sm:flex-row-reverse sm:px-6"
+                    >
                       <button
                         type="button"
                         className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm 
@@ -208,8 +181,9 @@ export default function Calendar1() {
                       </button>
                       <button
                         type="button"
-                        className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 
-                      shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                        className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2
+                         text-sm font-semibold text-gray-900  shadow-sm ring-1 ring-inset ring-gray-300
+                          hover:bg-gray-50 sm:mt-0 sm:w-auto"
                         onClick={handleCloseModal}
                       >
                         Cancel
